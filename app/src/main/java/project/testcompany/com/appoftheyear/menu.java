96f9d8d91521ad0d90622a9828d266ef38da0524
Lander <lander.vanlandeghem@student.ap.be>
2019-01-02 13:41:40 +0100

    bug fixes

--- app/src/main/java/project/testcompany/com/appoftheyear/menu.java
+++ app/src/main/java/project/testcompany/com/appoftheyear/menu.java
@@ -11,7 +11,7 @@ public class menu extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
+        setContentView(R.layout.mainmenu);
         //PlayGame();
     }
     /*public void PlayGame(){
