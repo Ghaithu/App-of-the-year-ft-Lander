abcbd9187f45d0b156ea7c5fd825f185d1793e00
Lander <lander.vanlandeghem@student.ap.be>
2019-01-02 14:26:19 +0100

    adding files

--- app/src/main/java/project/testcompany/com/appoftheyear/MainActivity.java
+++ app/src/main/java/project/testcompany/com/appoftheyear/MainActivity.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
