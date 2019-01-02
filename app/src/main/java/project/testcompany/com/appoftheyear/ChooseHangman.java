333717e05f662fa27f0f7e6cb2e8fcdb0ec31823
Ghaithu <30323454+Ghaithu@users.noreply.github.com>
2019-01-02 17:16:05 +0100

    no error only ugly layout

--- app/src/main/java/project/testcompany/com/appoftheyear/ChooseHangman.java
+++ app/src/main/java/project/testcompany/com/appoftheyear/ChooseHangman.java
@@ -8,6 +8,6 @@ public class ChooseHangman extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_choose_hangman);
+        setContentView(R.layout.hangman);
     }
 }
--- app/src/main/java/project/testcompany/com/appoftheyear/ChooseWordguess.java
+++ app/src/main/java/project/testcompany/com/appoftheyear/ChooseWordguess.java
@@ -8,6 +8,6 @@ public class ChooseWordguess extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_choose_wordguess);
+        setContentView(R.layout.wordguess);
     }
 }
